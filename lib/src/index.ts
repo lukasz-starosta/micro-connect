99f75bd24096acd17d2ea594e633c8bc7c99c8b6
Łukasz Starosta <lstarosta98@gmail.com>
2020-11-02 23:54:17 +0100

    Minor fixes after testing

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,2 @@
-export {default as ComponentManager} from './ComponentManager';
 export {default as ComponentWrapper} from './ComponentWrapper';
 export {default as Component, IComponentProps} from "./Component";
