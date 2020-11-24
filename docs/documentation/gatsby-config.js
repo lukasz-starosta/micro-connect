2639711a521e2715a4a85bc7ec5c609f3e91c186
Łukasz Starosta <lstarosta98@gmail.com>
2020-11-24 22:03:31 +0100

    Cleared up documentation

--- docs/documentation/gatsby-config.js
+++ docs/documentation/gatsby-config.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const queries = require("./src/utils/algolia");
 const config = require("./config");
 const plugins = [
   'gatsby-plugin-sitemap',
@@ -12,6 +11,7 @@ const plugins = [
   },
   'gatsby-plugin-emotion',
   'gatsby-plugin-react-helmet',
+  'gatsby-plugin-remove-serviceworker',
   {
     resolve: "gatsby-source-filesystem",
     options: {
@@ -36,46 +36,8 @@ const plugins = [
       ],
       extensions: [".mdx", ".md"]
     }
-  },
-  {
-    resolve: `gatsby-plugin-gtag`,
-    options: {
-      // your google analytics tracking id
-      trackingId: config.gatsby.gaTrackingId,
-      // Puts tracking script in the head instead of the body
-      head: true,
-      // enable ip anonymization
-      anonymize: false,
-    },
-  },
+  }
 ];
-// check and add algolia
-if (config.header.search && config.header.search.enabled && config.header.search.algoliaAppId && config.header.search.algoliaAdminKey) {
-  plugins.push({
-    resolve: `gatsby-plugin-algolia`,
-    options: {
-      appId: config.header.search.algoliaAppId, // algolia application id
-      apiKey: config.header.search.algoliaAdminKey, // algolia admin key to index
-      queries,
-      chunkSize: 10000, // default: 1000
-    }}
-  )
-}
-// check and add pwa functionality
-if (config.pwa && config.pwa.enabled && config.pwa.manifest) {
-  plugins.push({
-      resolve: `gatsby-plugin-manifest`,
-      options: {...config.pwa.manifest},
-  });
-  plugins.push({
-    resolve: 'gatsby-plugin-offline',
-    options: {
-      appendScript: require.resolve(`./src/custom-sw-code.js`),
-    },
-  });
-} else {
-  plugins.push('gatsby-plugin-remove-serviceworker');
-}
 
 // check and remove trailing slash
 if (config.gatsby && !config.gatsby.trailingSlash) {
