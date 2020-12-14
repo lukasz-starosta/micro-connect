fbdccc7f10972198ef1d52dbed4bc8ab7bac117e
Łukasz Starosta <lstarosta98@gmail.com>
2020-12-14 23:28:36 +0100

    Add on hover for notifications

--- docs/landing/src/notifications.js
+++ docs/landing/src/notifications.js
@@ -1,4 +1,12 @@
 import NotificationsContainer from '@lukasz-starosta/react-notifications';
 import '../node_modules/react-toastify/dist/ReactToastify.min.css';
 
-NotificationsContainer.mount(document.getElementById('notifications-container'));
+const container = document.getElementById('notifications-container');
+
+NotificationsContainer.mount(container, {rootClassName: 'component'});
+
+// target Toastify container
+container.children[0].addEventListener('DOMNodeInserted', (e) => {
+  // attach proper attributes to show on hover
+  e.target.dataset.componentMeta = `${NotificationsContainer.name} | v${NotificationsContainer.version}`
+});
