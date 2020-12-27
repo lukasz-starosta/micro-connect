c8c12e1270d6d79f78e685e7bf500c0f00c57610
Łukasz Starosta <lstarosta98@gmail.com>
2020-12-27 15:34:57 +0100

    Cleared up mounting elements

--- starters/javascript-starter/src/index.js
+++ starters/javascript-starter/src/index.js
@@ -1,21 +1,21 @@
 import {ComponentWrapper} from "@lukasz-starosta/micro-connect";
 import {name, version} from '../package.json';
 
-let element;
-
 function buildElement() {
-  element = document.createElement('p');
-  element.innerText = `JavaScript Template`;
+  const newElement = document.createElement('p');
+  newElement.innerText = `JavaScript Template`;
+
+  return newElement;
 }
 
+const element = buildElement();
+
 if (process.env.NODE_ENV === 'development') {
-  buildElement();
   document.body.appendChild(element);
 }
 
 export default new ComponentWrapper({
   mountElement: (parentElement, props) => {
-    buildElement();
     element.classList.add(props.rootClassName);
     element.id = props.id;
     parentElement.appendChild(element)
