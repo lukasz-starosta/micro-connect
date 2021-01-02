7e7f787e60b7063a6e8eca135b73aeb55950b5a3
Łukasz Starosta <lstarosta98@gmail.com>
2021-01-02 17:48:47 +0100

    Publish final page

--- docs/landing/src/navbar.js
+++ docs/landing/src/navbar.js
@@ -2,6 +2,7 @@ import Navbar from '@lukasz-starosta/js-navbar';
 
 const navbarContainer = document.createElement('div');
 navbarContainer.classList.add('component', 'component--navbar');
+console.log(Navbar.version);
 navbarContainer.dataset.componentMeta = `${Navbar.name} | v${Navbar.version}`;
 document.body.insertAdjacentElement('afterbegin', navbarContainer);
 
--- starters/javascript-starter/src/index.js
+++ starters/javascript-starter/src/index.js
@@ -1,21 +1,9 @@
 import {ComponentWrapper} from "@lukasz-starosta/micro-connect";
 import {name, version} from '../package.json';
 
-function buildElement() {
-  const newElement = document.createElement('p');
-  newElement.innerText = `JavaScript Template`;
-
-  return newElement;
-}
-
-const element = buildElement();
-
-if (process.env.NODE_ENV === 'development') {
-  document.body.appendChild(element);
-}
-
 export default new ComponentWrapper({
   mountElement: (parentElement, props) => {
+    const element = buildElement();
     element.classList.add(props.rootClassName);
     element.id = props.id;
     parentElement.appendChild(element)
